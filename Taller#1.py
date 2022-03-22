beac3b09aa04f4fca776dd59efb9f09836a43421
josue <josuenavarrol6gmail.com>
2022-03-22 09:43:15 -0500

    Ejercicio de python sin comentario terminado

--- Taller#1.py
+++ Taller#1.py
@@ -1,14 +1,11 @@
 class Cita :
-    #creo un atributo de clase para guardar el valor final 
     _valorFinal=None
 
-    #procedo a crear dicho constructor e inicializar  sus respectivos atributos
     def __init__(self, numero ,tipo,tarifa ):
         self._numero = numero
         self._tipo = tipo
         self._tarifa = tarifa
 
-    #creo los respectivo get de numero ,tarifa y tipo 
     def getNumero(self):
         return self._numero
     def getTarifa(self):
@@ -21,16 +18,13 @@ class Cita :
         else:
             return f"el tipo va de 1-5 y usted digito: {self._tipo}"
     def  calcularValorFinal(self):
-        #atrapo el tipo de la cita con el getTipo para no repetir la misma validacion hecha en getTipo, procedo a comparar cadenas
         tipo=self.getTipo()
         tipo1="General"
         tipo2="Especialista"
         if ( tipo == tipo1) :
-            #calculo el decremento de la tarifa y le doy un valor a _valorFinal
             decremento=self._tarifa*0.5
             self._valorFinal = self._tarifa - decremento
         elif (tipo == tipo2):
-            #calculo el Incremento de la tarifa y le doy un valor a _valorFinal
             incremento=self._tarifa*0.5
             self._valorFinal = self._tarifa + incremento
         else:
@@ -41,7 +35,6 @@ class Cita :
         f"Su tarifa es:\t{self._tarifa}\n"\
         f"Pero por se de tipo:{self.getTipo()} queda con un valor final de:{self._valorFinal}\t  \n"
 
-#procedo a crear un objeto con el tipo de cita Especialista  y llamo a sus respectivos metodos    
 cita1=Cita(100,5,4500)
 cita1.getTipo()
 cita1.calcularValorFinal()
@@ -49,7 +42,6 @@ print(cita1)
 
 print()
 
-#procedo a crear un objeto con el tipo de cita General y llamo a sus respectivos metodos
 cita2=Cita(100,1,4500)
 cita2.getTipo()
 cita2.calcularValorFinal()
--- Taller#2.py
+++ Taller#2.py
@@ -3,15 +3,12 @@ from random import  randrange
 
 
 class Bus :
-    #creo dos atributos de clase privados
     _pasajerosActuales=0
     _totalPasajeros=0
-    #procedo a crear el metodo constructor e inicializar sus respectivas variables de instancia
     def __init__(self,placa,capacidadPasajeros,precioPasaje):
         self._placa=placa
         self._capacidadPasajeros=capacidadPasajeros
         self._precioPasaje=precioPasaje
-    #creo los metodos get de placa,capacidad,precio pasaje ,pasajeros totales y pasajeros actuales        
     def getPlaca(self):
         return self._placa
     def getCapacidad(self):
@@ -22,7 +19,6 @@ class Bus :
         return self._totalPasajeros
     def getPasajerosActuales(self):
         return self._pasajerosActuales
-    #creo lo metodos subir pasajero y bajar pasajero (estos metodos modifican a _pasajerosActuales)
     def subirPasajeros(self):        
         if(self._pasajerosActuales<self._capacidadPasajeros): 
             self._pasajerosActuales += 1  
@@ -34,11 +30,9 @@ class Bus :
             print("no quedan pasajeros por bajar")
         else:
             self._pasajerosActuales -= 1
-    #creo el metodo que devuelve el total de todos los pasajes
     def getDineroAcumulado(self):
         return self._totalPasajeros*self._precioPasaje
 
-#creo el objeto y le paso sus respectivos argumentos
 buseta =Bus("xmn-opr",25,2500)
 
 #creo 2 test uno basico y otro donde lo hago de forma mas dinamica la subida y bajada de pasajeros ademas de que muestro los pasajeros actuales y cuantos subieron
@@ -52,13 +46,3 @@ buseta =Bus("xmn-opr",25,2500)
 # #mostramos por consola cuanto dinero se acumulo
 # print(f"el dinero acumulado del dia fue {buseta.getDineroAcumulado()} pesos")
 
-#----test2-----
-# for i in range (25):
-#     opcion=randrange(3)
-#     if opcion==1:
-#         buseta.subirPasajeros()
-#     else:
-#         buseta.bajarPasajeros()
-# print("la cantidad de pasajeros actuales es : ",buseta.getPasajerosActuales())
-# print("la cantidad de pasajeros totales es: ",buseta.getPasajerosTotales())
-# print(f"el dinero acumulado del dia fue {buseta.getDineroAcumulado()} pesos")
